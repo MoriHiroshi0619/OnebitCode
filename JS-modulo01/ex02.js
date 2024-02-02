6dd7ba77dad6e7822033023081ffba109b790d23
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-02-02 08:08:37 -0400

    updated

---
+++ JS-modulo01/ex02.js
@@ -0,0 +1,14 @@
+let nome = "Hiroshi";
+
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+window.alert(nome);
+nome = window.prompt("Insira seu nome");
+console.log(nome);
+
+let confirma = window.confirm("confirma essa ação");
+console.log(confirma);
+
+alert("olá mundo!");
