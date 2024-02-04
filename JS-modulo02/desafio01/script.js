99680f375ca59aac40eaa67b1ce5518d7ecc78dd
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-02-03 23:23:10 -0400

    updated

---
+++ JS-modulo02/desafio01/script.js
@@ -0,0 +1,23 @@
+let opcao;
+const pacientes = [];
+do{
+    opcao = prompt("\nPor favor escolha uma ação\n1 - agendar consulta\n2 - consutar paciente \n3 - sair");
+    console.log(opcao);
+    switch(opcao){
+        case "1":
+            let paciente = prompt("Informe o Nome do Paciente");
+            pacientes.push(paciente);
+            console.log(pacientes);
+            break;
+        case "2":
+            pacientes.shift();
+            console.log(pacientes);
+            break;
+        case "3":
+            alert("Obrigado");
+            break;
+        default:
+            alert("Por favor insira uma opção válida");
+            break;
+    }
+}while(opcao != 3);
