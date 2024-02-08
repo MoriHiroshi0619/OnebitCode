c771de636d5d53095c3316cf1776593165421e29
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-02-07 23:11:04 -0400

    updated

---
+++ projetos/tic_tac_toe/js/script.js
@@ -0,0 +1,22 @@
+//Classe do X: bi bi-x-lg
+//Classe da Bola: bi bi-circle
+let player1 = {};
+let player2 = {};
+
+
+/* const startBtn = document.getElementById('startGameBtn');
+
+startBtn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    const player1name = document.getElementById('player1name').value;
+    const player2name = document.getElementById('player2name').value;
+
+    if(player1name.length != 0 && player2name.length != 0){
+        document.getElementById('playerForm').remove();
+        player1['nome'] = player1name;
+        player2['nome'] = player2name;
+    } else {
+        alert('Por favor preencha os nomes do jogadores corretamente');
+    }
+    console.log(player1, player2)
+}) */
