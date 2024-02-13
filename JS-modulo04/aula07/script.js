01ec4045c8aa82e06b49bc04819a7c6206e9a0a5
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-02-13 18:24:54 -0400

    updated

---
+++ JS-modulo04/aula07/script.js
@@ -0,0 +1,12 @@
+import {inline as i} from "./inline.js"
+
+import defaultInline from "./inline.js";
+
+import { group } from "./non-inline.js";
+
+import b from "./non-inline.js"
+i();
+defaultInline()
+
+group()
+b()
