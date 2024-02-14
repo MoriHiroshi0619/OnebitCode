0ee6b471df5915772958996fe0ea884823e3b78b
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-02-13 23:28:28 -0400

    updated

---
+++ JS-modulo04/aula08/index.js
@@ -0,0 +1,5 @@
+const lodash = require("lodash");
+const command = require("nodemon/lib/config/command");
+
+console.log(lodash.isArray([]));
+console.log(lodash.kebabCase("Anderson Hiroshi Mori Correia"));
