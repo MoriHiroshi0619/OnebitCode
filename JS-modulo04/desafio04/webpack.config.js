95cd4a826720844889cebe42991bd5456c47b8ae
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2024-02-16 12:27:55 -0400

    updated

--- JS-modulo04/desafio04/webpack.config.js
+++ JS-modulo04/desafio04/webpack.config.js
@@ -1,6 +1,11 @@
 const miniCssPlugin = require('mini-css-extract-plugin');
-
+const path = require('path');
 module.exports = {
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+    },
     entry:{
         script: './src/script.js',
         /* calcular: './src/JS/calcular.js',
